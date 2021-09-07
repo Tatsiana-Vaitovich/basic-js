ac1905cc602b52ec8cd3a2f0c8274030f073140c
Tatsiana <t_a_b@tut.by>
2021-09-07 17:47:00 +0300

    init: start basic-js task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,10 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function contCats(matrix) {
+  if (matrix.length === 0) {
+    return 0;
+  }
+  matrix = matrix.map(el => el.filter(item => item === "^^").length)
+  return matrix.reduce((summ, current) => summ + current);
 }
